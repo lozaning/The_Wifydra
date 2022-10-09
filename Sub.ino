5c06b622a4cb1a56ef67b2cad5f359412c2708bf
lozaning <lozaning@gmail.com>
2022-10-09 18:30:14 -0500

    Add files via upload
    This gets flashed onto all the sub sesnors. Make not that you need to change the myData.b = 2; to a different integer on everyboard before flashing.

---
+++ Sub.ino
@@ -0,0 +1,103 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp8266-nodemcu-arduino-ide/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+// REPLACE WITH RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  char a[32];
+  int b;
+  float c;
+  //String d;
+  bool e;
+} struct_message;
+
+String AP;
+
+// Create a struct_message called myData
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 2000;  // send readings timer
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Last Packet Send Status: ");
+  if (sendStatus == 0) {
+    Serial.println("Delivery success");
+  }
+  else {
+    Serial.println("Delivery fail");
+  }
+}
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+
+void loop() {
+  char Buf[50];
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+
+    //myData.b = random(1,20);
+    //myData.c = 1.2;
+    int n = WiFi.scanNetworks();
+    if (n == 0) {
+      Serial.println("No networks found");
+      Serial.println("No networks found");
+    } else {
+      for (int8_t i = 0; i < n; i++) {
+        AP = WiFi.SSID(i);
+        Serial.println(AP);
+        AP.toCharArray(Buf, 50);
+        strcpy(myData.a, Buf);
+        myData.e = false;
+        myData.b = 2l;
+        myData.c = 1.2;
+        esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+        delay(400);
+      }
+    }
+    //strcpy(myData.a, Buf);
+    //////myData.d = AP;
+    //myData.e = false;
+
+    // Send message via ESP-NOW
+    //esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+
+    lastTime = millis();
+  }
+}
